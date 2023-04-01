396a2f8a44324dedbad9de30d9cfed7714dc599d
podlaski <live.m851@gmail.com>
2023-04-02 00:05:10 +0200

    Przekazywanie propsów do style compontents

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({secondary}) => secondary ? '#e6e6e6' : '#ffd82b'};
-  width: ${({secondary}) => secondary ? '110px' : '220px'};
-  height: ${({secondary}) => secondary ? '30px' : '47px'};
+  background-color: #ffd82b;
+  width: 220px;
+  height: 47px;
   border: none;
   border-radius: 50px;
-  font-size: ${({secondary}) => secondary ? '10px' : '16px'};
+  font-size: 16px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
   text-transform: uppercase;
+
+  ${({secondary}) => (
+    secondary && css`
+    background-color: #e6e6e6;
+    width: 110px;
+    height: 30px;
+    font-size: 16px;
+    `
+  )}
 `;
 
 export default Button;
