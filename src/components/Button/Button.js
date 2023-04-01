701d28bb25567cacd27beb8f9cfd6c0ba82f7b9b
podlaski <live.m851@gmail.com>
2023-04-01 23:52:56 +0200

    Stworzenie komponentu dla button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #ffd82b;
+  width: 220px;
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-size: 16px;
+  font-family: "montserrat";
+  font-weight: 500;
+  text-transform: uppercase;
+`;
+
+export default Button;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
+import Button from '../../components/Button/Button';
 
 const App = styled.h1`
   font-size: 112px;
   color: red;
 `;
 
-const Root = () => <App>Siemano</App>;
+const Root = () => (
+  <>
+    <Button>Close / save</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
 
 export default Root;
