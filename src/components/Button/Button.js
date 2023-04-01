65acc11039a23f3bffc885e42778ed73b43bffc5
podlaski <live.m851@gmail.com>
2023-04-01 23:58:55 +0200

    Napisanie styli dla button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background-color: #ffd82b;
-  width: 220px;
-  height: 47px;
+  background-color: ${({secondary}) => secondary ? '#e6e6e6' : '#ffd82b'};
+  width: ${({secondary}) => secondary ? '110px' : '220px'};
+  height: ${({secondary}) => secondary ? '30px' : '47px'};
   border: none;
   border-radius: 50px;
-  font-size: 16px;
-  font-family: "montserrat";
-  font-weight: 500;
+  font-size: ${({secondary}) => secondary ? '10px' : '16px'};
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
   text-transform: uppercase;
 `;
 
